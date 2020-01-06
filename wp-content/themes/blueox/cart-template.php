f689b2476baf204ef4b248b4a71f8c64526bf367
veadev <ayushi.sharma@kelltontech.org>
2020-01-06 16:03:02 +0530

    changes undo

--- wp-content/themes/blueox/cart-template.php
+++ wp-content/themes/blueox/cart-template.php
@@ -1,12 +1,7 @@
 <?php
 /*
 Template Name: Cart */
-get_header(); 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-?>
+get_header(); ?>
 
 <?php
 	$default_sidebar_position = get_theme_mod( 'default_sidebar_position', 'right' );
