c4219626be6e008edd81d6fd37bcd5ae4ed049c9
Prabhjot Singh <prabhjot.singh@kelltontech.org>
2020-01-13 12:58:39 +0530

    Orders page, order details page and category page

--- wp-content/themes/blueox/woocommerce/archive-product.php
+++ wp-content/themes/blueox/woocommerce/archive-product.php
@@ -31,7 +31,7 @@ do_action( 'woocommerce_before_main_content' );
 ?>
 <header class="woocommerce-products-header">
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+		<!-- <h1 class="woocommerce-products-header__title page-title"><?php //woocommerce_page_title(); ?></h1> -->
 	<?php endif; ?>
 
 	<?php
@@ -41,53 +41,126 @@ do_action( 'woocommerce_before_main_content' );
 	 * @hooked woocommerce_taxonomy_archive_description - 10
 	 * @hooked woocommerce_product_archive_description - 10
 	 */
-	do_action( 'woocommerce_archive_description' );
+	//do_action( 'woocommerce_archive_description' );
 	?>
+	<div class="category-header">
+		<div class="category-header-col1">
+			<h3>The BLUE OX Difference</h3>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ut odio omnis recusandae quasi magni excepturi temporibus exercitationem quia. Tempora cumque expedita corrupti rem officia! Reprehenderit blanditiis debitis laudantium eum?</p>
+		</div>
+		<div class="category-header-col2">
+			<h3>Need a LOCAL Dealer?</h3>
+			<p>There are 100's to choose from...</p>
+			<a href="#" class="btn-blue">Find a Dealer</a>
+		</div>
+	</div>
 </header>
-<?php
-if ( woocommerce_product_loop() ) {
-
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked woocommerce_output_all_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	do_action( 'woocommerce_before_shop_loop' );
-
-	woocommerce_product_loop_start();
-
-	if ( wc_get_loop_prop( 'total' ) ) {
-		while ( have_posts() ) {
-			the_post();
+<div class="product-category-wrap">
+	<div class="product-category-filter">
+		<?php 
+			/**
+			 * Hook: woocommerce_sidebar.
+			 *
+			 * @hooked woocommerce_get_sidebar - 10
+			 */
+			do_action( 'woocommerce_sidebar' );
+		?>
+	</div>
+	<div class="product-category-list">
+		<?php
+		if ( woocommerce_product_loop() ) {
 
 			/**
-			 * Hook: woocommerce_shop_loop.
+			 * Hook: woocommerce_before_shop_loop.
+			 *
+			 * @hooked woocommerce_output_all_notices - 10
+			 * @hooked woocommerce_result_count - 20
+			 * @hooked woocommerce_catalog_ordering - 30
 			 */
-			do_action( 'woocommerce_shop_loop' );
+			//do_action( 'woocommerce_before_shop_loop' );
 
-			wc_get_template_part( 'content', 'product' );
+			//woocommerce_product_loop_start();
+
+			if ( wc_get_loop_prop( 'total' ) ) {
+				while ( have_posts() ) {
+					the_post();
+
+					/**
+					 * Hook: woocommerce_shop_loop.
+					 */
+					do_action( 'woocommerce_shop_loop' ); ?>
+				<div class="product-list-item">
+						<div class="product-list-img">
+					<?php
+						/**
+						 * Hook: woocommerce_before_shop_loop_item.
+						 *
+						 * @hooked woocommerce_template_loop_product_link_open - 10
+						 */
+						do_action( 'woocommerce_before_shop_loop_item' ); ?>
+						<?php
+
+						/**
+						 * Hook: woocommerce_before_shop_loop_item_title.
+						 *
+						 * @hooked woocommerce_show_product_loop_sale_flash - 10
+						 * @hooked woocommerce_template_loop_product_thumbnail - 10
+						 */
+						do_action( 'woocommerce_before_shop_loop_item_title' ); ?>
+						</div>
+						<div class="product-list-content">
+							<?php
+								/**
+								 * Hook: woocommerce_shop_loop_item_title.
+								 *
+								 * @hooked woocommerce_template_loop_product_title - 10
+								 */
+								do_action( 'woocommerce_shop_loop_item_title' );
+
+								/**
+								 * Hook: woocommerce_after_shop_loop_item_title.
+								 *
+								 * @hooked woocommerce_template_loop_rating - 5
+								 * @hooked woocommerce_template_loop_price - 10
+								 */
+								do_action( 'woocommerce_after_shop_loop_item_title' );
+
+								/**
+								 * Hook: woocommerce_after_shop_loop_item.
+								 *
+								 * @hooked woocommerce_template_loop_product_link_close - 5
+								 * @hooked woocommerce_template_loop_add_to_cart - 10
+								 */
+								do_action( 'woocommerce_after_shop_loop_item' );
+								?>
+								<div class="learn-btn">
+									<a href="#" class="btn-blue">Learn More</a>
+								</div>
+						</div>
+					</div>
+			<?php	}
+			}
+
+			//woocommerce_product_loop_end();
+
+			/**
+			 * Hook: woocommerce_after_shop_loop.
+			 *
+			 * @hooked woocommerce_pagination - 10
+			 */
+			do_action( 'woocommerce_after_shop_loop' );
+		} else {
+			/**
+			 * Hook: woocommerce_no_products_found.
+			 *
+			 * @hooked wc_no_products_found - 10
+			 */
+			do_action( 'woocommerce_no_products_found' );
 		}
-	}
-
-	woocommerce_product_loop_end();
-
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop' );
-} else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action( 'woocommerce_no_products_found' );
-}
-
+		?>
+	</div>
+</div>
+<?php
 /**
  * Hook: woocommerce_after_main_content.
  *
@@ -100,6 +173,6 @@ do_action( 'woocommerce_after_main_content' );
  *
  * @hooked woocommerce_get_sidebar - 10
  */
-do_action( 'woocommerce_sidebar' );
+//do_action( 'woocommerce_sidebar' );
 
 get_footer( 'shop' );
